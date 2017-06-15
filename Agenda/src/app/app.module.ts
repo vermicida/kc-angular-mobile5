ac8aed29bb27854f7586ad8c3994e7bb42f333a5
Diego Herrera <vermicida@gmail.com>
2017-06-15 21:22:41 +0200

    Repasamos nuestro AppModule.

--- Agenda/src/app/app.module.ts
+++ Agenda/src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  // En 'declarations' indicamos todos los componentes, pipes y
+  // directivas de mi aplicación.
   declarations: [
     AppComponent
   ],
+  // En 'imports' indicamos módulos de los cuáles dependemos.
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  // En 'providers' indicamos los proveedores de todas aquellas
+  // piezas que sean susceptibles de ser inyectadas como dependencias.
+  providers: [
+  ],
+  // En 'bootstrap' indicamos el componente raíz, que es el primero
+  // que se instanciará y hará lo propio con el resto.
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
